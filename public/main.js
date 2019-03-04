75ad4f5f0326616ada7a38b734df6e89498dd222
Rich Hildred <rhildred@gmail.com>
2019-03-04 21:47:03 +0000

    put this back to the way that I remember it from class

--- public/main.js
+++ public/main.js
@@ -1,9 +1,36 @@
+import config from "./firebase.js";
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+function getFromFirebase() {
+    firebase.database().ref('todos').on("value", snapshot =>{
+        let oTodos = snapshot.val();
+        let oTaskList = document.getElementById("tasklist");
+        console.log(oTodos);
+        oTaskList.innerHTML = "";
+        Object.keys(oTodos).map((key) => {
+            let oElement = document.createElement("p");
+            oElement.innerHTML = oTodos[key].taskName;
+            oTaskList.prepend(oElement);
+        });
+    });
+}
+
 window.addEventListener("load", ()=>{
+    //document is loaded now
+    firebase.initializeApp(config);
+    getFromFirebase();
+
     document.getElementById("taskForm").addEventListener("submit", (evt)=>{
-        let sTask = document.getElementById("task").value;
-        let oElement = document.createElement("p");
-        oElement.innerHTML = sTask;
-        document.getElementById("tasklist").prepend(oElement);
         evt.preventDefault();
+        let sTask = document.getElementById("task").value;
+        document.getElementById("task").value = "";
+        let taskID = new Date().toISOString().replace(".", "_");
+        firebase.database().ref('todos/' + taskID).set({
+            taskName: sTask
+        }).then(() => {
+            console.log("inserted");
+        });
+
     });
 })
