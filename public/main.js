709e8099953b96a08543b492375bb97a36007280
rhildred <rhildred@gmail.com>
2019-03-08 11:52:56 -0500

    finished crud

--- public/main.js
+++ public/main.js
@@ -9,25 +9,46 @@ function getFromFirebase() {
         console.log(oTodos);
         oTaskList.innerHTML = "";
         Object.keys(oTodos).map((key) => {
-            let oElement = document.createElement("p");
+            let oElement = document.createElement("div");
+            oElement.className = "col-md-6";
             oElement.innerHTML = oTodos[key].taskName;
-            oElement.innerHTML += "<button class=\"deleteTask\" id=\"" + key + "\">x</button>";
-            oTaskList.prepend(oElement);
+            let oButton = document.createElement("button");
+            oButton.innerHTML = "x";
+            oButton.className = "col-md-3";
+            oButton.addEventListener("click", deleteTodo);
+            let oComplete = document.createElement("button");
+            oComplete.innerHTML = "&#10004;";
+            oComplete.className = "col-md-3";
+            oComplete.addEventListener("click", completeTodo);
+            let oRow = document.createElement("div");
+            oRow.className = "row"
+            oRow.id = key;
+            oRow.append(oElement);
+            oRow.append(oButton);
+            oRow.append(oComplete);
+            oTaskList.prepend(oRow);
         });
     });
 }
 
-function deleteTodo(sKey){
-    if(sKey){
-        alert("deleted" + sKey);
-    }
+function deleteTodo(evt){
+    let sId = evt.target.parentNode.id;
+    firebase.database().ref('todos/' + sId).remove(()=>{
+        console.log("removed " + sId);
+    });
+}
+
+function completeTodo(evt){
+    let sId = evt.target.parentNode.id;
+    firebase.database().ref('todos/' + sId + "/dateCompleted").set(new Date().toISOString(), ()=>{
+        console.log("completed " + sId);
+    });
 }
 
 window.addEventListener("load", ()=>{
     //document is loaded now
     firebase.initializeApp(config);
     getFromFirebase();
-    deleteTodo();
     document.getElementById("taskForm").addEventListener("submit", (evt)=>{
         evt.preventDefault();
         let sTask = document.getElementById("task").value;
